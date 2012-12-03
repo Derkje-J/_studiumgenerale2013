1b49a4174c8302dd934f30c6c99844768a353191
Derk-Jan Karrenbeld <derk-jan@karrenbeld.info>
2012-12-03 02:44:31 +0100

    Added calendar and front sidebars

---
+++ sidebar-calendar.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The Sidebar with the calendar for the front page
+ *
+ * @package Studium Generale 2013
+ * @since Studium Generale 2013 1.0
+ */
+?>
+		<div id="secondary" class="fourcol last" role="complementary">
+            <aside id="calendar" class="widget widget_calendar calendar">
+            </aside>
+            
+            <aside id="recordings" class="widget widget_recordings">
+            	<h3 class="widget-title"><?php _e("Recordings of recent events", "sg2013"); ?></h3>
+	            <ul class="recordings">
+            		<li class="recording">
+                    	<a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'sg2013' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
+                    </li>
+                    <li class="recording">
+                    	<a href="#" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark">Some other event</a>
+                    </li>
+                    <li class="recording">
+                    	<a href="#" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark">Some event</a>
+                    </li>
+            	</ul>
+            </aside>
+		</div><!-- #secondary .widget-area -->
